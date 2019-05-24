c0fd084f5fd4dc1706e759dd5975eede78b628ff
Maciej Budziński <maciej.budzinski@netguru.pl>
2019-05-24 11:32:20 +0200

    add animations

--- lib/main.dart
+++ lib/main.dart
@@ -10,77 +10,88 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  MyHomePage({Key key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List data;
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  // List data;
+
+  @override
+  void initState() {
+    animationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 500));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    animationController.repeat();
+
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
         body: new Container(
-          child: new Center(
-              child: new FutureBuilder(
-            future:
-                DefaultAssetBundle.of(context).loadString('data/shows.json'),
-            builder: (context, snapshot) {
-              var data = jsonDecode(snapshot.data.toString());
-              return new ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return new Card(
+      child: new Center(
+          child: new FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString('data/shows.json'),
+        builder: (context, snapshot) {
+          var data = jsonDecode(snapshot.data.toString());
+          return new ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return new Card(
+                  child: new Padding(
+                      padding: EdgeInsets.all(10.0),
                       child: new Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[new Text("Id" + data[index]["name"])],
-                  ));
-                },
-                itemCount: data == null ? 0 : data.length,
-              );
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          new Text(
+                            data[index]["name"],
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          new Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: new RotationTransition(
+                              turns: Tween(begin: 0.0, end: 1.0)
+                                  .animate(animationController),
+                              child: new Text("S" +
+                                  data[index]["season"].toString() +
+                                  "E" +
+                                  data[index]["number"].toString()),
+                            ),
+                          ),
+                          new Image.network(
+                            data[index]['image']['medium'],
+                            height: 200,
+                            width: 300,
+                            fit: BoxFit.cover,
+                          )
+                        ],
+                      )));
             },
-          )),
-        ));
+            itemCount: data == null ? 0 : data.length,
+          );
+        },
+      )),
+    ));
   }
 }
 
