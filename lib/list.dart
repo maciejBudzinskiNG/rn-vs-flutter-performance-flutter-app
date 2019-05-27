eb8ac767e9dae5b1de75ddc9c2470ed8be16f704
Maciej Budziński <maciej.budzinski@netguru.pl>
2019-05-27 15:35:50 +0200

    refactor list view

---
+++ lib/list.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Episodes extends StatelessWidget {
+  final List episodes;
+  final AnimationController animationController;
+  Episodes(this.episodes, this.animationController);
+
+  Widget _buildItem(BuildContext context, int index) {
+    String name = episodes[index]['name'];
+    String season = episodes[index]["season"].toString();
+    String episode = episodes[index]["number"].toString();
+    String imgUrl = episodes[index]['image']['medium'];
+
+    return Card(
+        child: new Padding(
+            padding: EdgeInsets.all(10.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  '${index + 1}. $name',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                new Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: new RotationTransition(
+                    turns: Tween(begin: 0.0, end: 1.0)
+                        .animate(animationController),
+                    child: new Text('S${season}E$episode'),
+                  ),
+                ),
+                new Image.network(
+                  imgUrl,
+                  height: 200,
+                  width: 300,
+                  fit: BoxFit.cover,
+                )
+              ],
+            )));
+  }
+
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemBuilder: _buildItem,
+        itemCount: episodes == null ? 0 : episodes.length);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:async';
+
+import 'package:flutter_app/list.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,12 +30,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   AnimationController animationController;
-  // List data;
+  List data;
+
+  loadJsonData() async {
+    var jsonText = await rootBundle.loadString(('data/shows.json'));
+    setState(() {
+      data = json.decode(jsonText);
+    });
+  }
 
   @override
   void initState() {
     animationController = new AnimationController(
         vsync: this, duration: Duration(milliseconds: 500));
+
+    this.loadJsonData();
     super.initState();
   }
 
@@ -47,68 +60,7 @@ class _MyHomePageState extends State<MyHomePage>
 
     return Scaffold(
         body: new Container(
-      child: new Center(
-          child: new FutureBuilder(
-        future: DefaultAssetBundle.of(context).loadString('data/shows.json'),
-        builder: (context, snapshot) {
-          var data = jsonDecode(snapshot.data.toString());
-          return new ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              return new Card(
-                  child: new Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          new Text(
-                            data[index]["name"],
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          new Padding(
-                            padding: EdgeInsets.all(15.0),
-                            child: new RotationTransition(
-                              turns: Tween(begin: 0.0, end: 1.0)
-                                  .animate(animationController),
-                              child: new Text("S" +
-                                  data[index]["season"].toString() +
-                                  "E" +
-                                  data[index]["number"].toString()),
-                            ),
-                          ),
-                          new Image.network(
-                            data[index]['image']['medium'],
-                            height: 200,
-                            width: 300,
-                            fit: BoxFit.cover,
-                          )
-                        ],
-                      )));
-            },
-            itemCount: data == null ? 0 : data.length,
-          );
-        },
-      )),
+      child: new Center(child: Episodes(data, animationController)),
     ));
-  }
-}
-
-class Episode {
-  final String id;
-  final int season;
-  final int number;
-  final String imageUrl;
-
-  Episode({this.id, this.season, this.number, this.imageUrl});
-
-  factory Episode.fromJson(Map<String, dynamic> json) {
-    return new Episode(
-      id: json['id'].toString(),
-      season: json['season'],
-      number: json['number'],
-      imageUrl: json['image'],
-    );
   }
 }
